edf130de7cd44d1754bb7da5c16a4b0c4c17d9d7
sjain76 <41167107+sjain76@users.noreply.github.com>
2018-12-27 14:18:51 -0600

    Add files via upload

---
+++ amazon left cat scrape.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 20 00:34:35 2018
+
+@author: sidharthjain
+"""
+
+from lxml import html
+import requests
+import pandas as pd
+import numpy as np 
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+from contextlib import suppress
+from time import sleep
+from random import randint
+from time import time
+xlsx1 = pd.ExcelFile("/Users/sidharthjain/Downloads/pandas_simple2.xlsx")
+xlsx1
+df = xlsx1.parse("Sheet1")
+links=df["Link to page"]
+links1=links[0:10000]
+links1
+
+links_main=[]
+count=[]
+for index, i in enumerate(links1):
+    with suppress(Exception):
+        
+    #page=Request(i, headers={'User-Agent': 'Mozilla/5.0'})
+    #page1=urlopen(page).read()
+        #print(i)
+        #agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+        headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36)'}
+        page = requests.get(i,headers=headers)
+        print(index)
+        sleep(randint(10,15))
+        
+        #requests+=1
+        #elapsed_time=time()-start_time
+        #print('Request:{}; Frequency: {} requests/s'.format(requests, requests/elapsed_time))
+        #clear_output(wait = True)
+        
+        contents = page.content 
+        #print(contents)
+        soup = BeautifulSoup(contents, "lxml")
+        soup.prettify()
+        a=soup.find('span',attrs={'id':'s-result-count'})
+        product_name=a.text
+        print(product_name)
+        links_main.append(i)
+        count.append(product_name)
+        
+df1 = pd.DataFrame({'cat/subcat link': links_main, 'sku count': count})
+df1
+writer = pd.ExcelWriter('pandas_simple3.xlsx', engine='xlsxwriter',options={'strings_to_urls': False})
+df1.to_excel(writer, sheet_name='Sheet1')
+
+writer.save()
+        
+product_name
